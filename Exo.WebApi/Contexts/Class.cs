599a1da12e8c753883181f4c449fc11091c8f24d
Rafael Silva <rafa.tuktuk@hotmail.com>
2025-10-07 20:34:32 -0300

    ExoContext pronto

---
+++ Exo.WebApi/Contexts/Class.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext: DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=HOUSTONTrusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
--- Exo.WebApi/Program.cs
+++ Exo.WebApi/Program.cs
@@ -1,19 +1,19 @@
-// using Exo.WebApi.Contexts;
-// using Exo.WebApi.Repositories;
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
 
-// var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddScoped<ExoContext, ExoContext>();
-// builder.Services.AddControllers();
-// builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+builder.Services.AddScoped<ExoContext, ExoContext>();
+builder.Services.AddControllers();
+builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 
-// var app = builder.Build();
+var app = builder.Build();
 
-// app.UseRouting();
+app.UseRouting();
 
-// app.UseEndpoints(endpoints =>
-// {
-//     endpoints.MapControllers();
-// });
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
-// app.Run();
+app.Run();
